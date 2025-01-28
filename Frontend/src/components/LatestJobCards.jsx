98fe9fc534b416e4363a8d8da77f594954d2900d
om <omjha003@gmail.com>
2025-01-28 18:12:10 +0530

    Fixed some bugs of the site

--- Frontend/src/components/LatestJobCards.jsx
+++ Frontend/src/components/LatestJobCards.jsx
@@ -3,14 +3,14 @@ import { Badge } from "@/components/ui/badge"
 
 export default function LatestJobCards({ job }) {
   return (
-    <div className='p-5 rounded-md shadow-xl border border-gray-100 cursor-pointer'>
+    <div className='p-10 rounded-md shadow-xl border border-gray-100 cursor-pointer'>
         <div>
-            <h1 className='text-3xl font-normal'>{job?.companyId?.name}</h1> 
-            <p className='text-sm text-gray-500'>{job.location}</p>
+            <h1 className='text-3xl font-medium'>{job?.companyId?.name}</h1> 
+            <p className='text-sm my-2 text-gray-500'>{job.location}</p>
         </div>
 
         <div className='my-2'>
-            <h1 className='text-2xl font-bold mb-2'>{job.title}</h1>
+            <h1 className='text-2xl font-normal mb-2'>{job.title}</h1>
             <p className='text-sm text-gray-600'>{job.description}</p>
         </div>
 
--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
         <div className='bg-white w-[100vw]'>
             <div className='flex items-center justify-between max-w-7xl mx-auto h-16 '>
                 <div>
-                    <h1 className="text-2xl font-bold">Job <span className='text-red-700'>Portal</span></h1>
+                    <h1 className="text-2xl font-bold cursor-pointer" onClick={() => navigate("/")}>Job <span className='text-red-700'>Portal</span></h1>
                 </div>
 
                 <div className='flex items-center gap-12'>
